87f62b5f5ddd291f00d8c7ae4716442831e475f3
IamFriendly <ryanr@ohivey.com>
2019-01-17 10:47:23 -0700

    fixed the login, I think

---
+++ web/login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<p>testing123</p>
+		<div>
+			<input type=button>Login as Administrator</button>
+		</div>
+		<div>
+			<input type=button>Login as Tester</button>
+		</div>
+	</body>
+</html>
