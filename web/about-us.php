02947ad0ae31371f644738275130c451ce3e69c2
IamFriendly <ryanr@ohivey.com>
2019-01-17 10:34:25 -0700

    files for team project

---
+++ web/about-us.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<div>
+			<p>Hello, we are the leading provider of pictures of cats on pillows. If you are looking for maximum cuteness, you have come to the right place.</p>
+		</div>
+	</body>
+</html>
---
+++ web/home.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<div>
+			<p>Welcome, you are not logged in.</p>
+		</div>
+	</body>
+</html>
