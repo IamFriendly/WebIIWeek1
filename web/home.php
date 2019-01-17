3f0d4a04ec9d74882d6b9fe6963e8e044e4da333
IamFriendly <ryanr@ohivey.com>
2019-01-17 11:14:55 -0700

    home link

--- web/home.php
+++ web/home.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<body>
-		<?php include'navbar.php'; ?>
+		<?php $home = "active"; include'navbar.php'; ?>
 		<div>
 			<p>Welcome, you are not logged in.</p>
 		</div>
--- web/navbar.php
+++ web/navbar.php
@@ -1,6 +1,6 @@
 <nav>
 	<ul>
-		<li>Home</li>
+		<li><a href = "home.php" class="<?php echo $home?>">Home</a></li>
 		<li>Login</li>
 		<li>About Us</li>
 	</ul>
