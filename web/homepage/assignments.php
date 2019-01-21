bfbb61471685b63b8bf27bfa32763b9dc30b0f65
IamFriendly <ryanr@ohivey.com>
2019-01-21 13:22:08 -0700

    assignments

---
+++ web/homepage/assignments.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
+<body>
+	<div id=nav>
+		<?php $assignments="active"; include'header.php'; ?>
+	</div>
+	<div id=mainContent>
+		<h1>More Coming Soon!</h1>
+	</div>
+</body>
+</html>
