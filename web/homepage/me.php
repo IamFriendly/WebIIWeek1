72ccc5ee23704b92997fbec4a7c55591dbe93a6a
IamFriendly <ryanr@ohivey.com>
2019-01-21 12:41:04 -0700

    stuff for homepage assignment phase one

---
+++ web/homepage/me.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
+<body>
+	<?php $home="active"; include'header.php'; ?>
+	<div id="mainContent">
+		<h1>Who Am I</h1>
+		<h2>Brief History</h2>
+		<p>I was born in Hawaii, lived a couple of years in Virginia, but mostly raised in Utah. There I went to three elementary schools, two middle schools, and one high school. I graduated from Alta High in 2012 with a class of about a thousand. I went on a mission to California as one of the first 18 year old missionaries. I went to LDSBC for a year, then SLCC for a semester, then came to BYUI. I started as a business management major, then accoutning, then computer science, then software engineering. I hope to graduate this semester.</p><br>
+		<h2>Interests</h2>
+		<p>I like games, and I play a number of them. I'm pretty big into Magic the Gathering. I frequently go to large tournaments with money on the line. I play League of Legends if I'm looking to just hang out with friends. I play Osu! as a challenging rythm game that rewards improvment.</p>
+	</div>
+</body>
