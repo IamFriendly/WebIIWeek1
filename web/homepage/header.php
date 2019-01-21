1752288f94443252756e6e8ef712e6e13745db27
IamFriendly <ryanr@ohivey.com>
2019-01-21 12:44:34 -0700

    navbar

---
+++ web/homepage/header.php
@@ -0,0 +1,6 @@
+<nav>
+	<ul id="navbar">
+		<li><a href = "me.php" class="<?php echo $home?>">Home</a></li>
+		<li><a href="assignemnts.php" class="<?php echo $assignments?>">Assignments</li>
+	</ul>
+</nav>
